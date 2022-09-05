76d44cf280cc099a6c53a1c0c2fe6c60a25f08bb
amranhossain <amranhossainchy@gmail.com>
2022-09-05 21:05:00 +0600

    finished

---
+++ dom.js
@@ -0,0 +1,18 @@
+document.getElementById('yellow').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'yellow';
+});
+document.getElementById('royal-blue').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'royalblue';
+});
+document.getElementById('indian-red').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'indianred';
+});
+document.getElementById('aquamarine').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'aquamarine';
+});
+document.getElementById('golden-rod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+});
+document.getElementById('default').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'white';
+});
